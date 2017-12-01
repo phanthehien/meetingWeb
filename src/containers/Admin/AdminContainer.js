1ca2af4a05eeaf17a4ece58f11258c878c6477d9
Hien Phan The <phanthehien@gmail.com>
2017-12-02 00:56:15 +0700

    add the manage page

--- src/containers/Admin/AdminContainer.js
+++ src/containers/Admin/AdminContainer.js
@@ -1,37 +1,106 @@
 import React from 'react'
-import { Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
+import { Divider, Dropdown, Icon, Button } from 'semantic-ui-react'
+
+
+const style = {
+  plainContainer: {
+    padding: 20,
+    border: 1
+  },
+};
+
+const DropdownExampleMenuDirection = () => (
+  <Dropdown floating button className='icon'>
+    <Dropdown.Menu>
+      <Dropdown.Item>
+        <Icon name='star' />
+        <span className='text'>Mark as good</span>
+      </Dropdown.Item>
+      <Dropdown.Item>
+        <Icon name='edit' />
+        <span className='text'>Edit</span>
+      </Dropdown.Item>
+      <Dropdown.Item>
+        <Icon name='trash' />
+        <span className='text'>Delete</span>
+      </Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+)
 
 const AdminContainer = () => (
-  <div className='login-form'>
-    {/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-    <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-    <Grid
-      textAlign='center'
-      style={{ height: '100%' }}
-      verticalAlign='middle'
-    >
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'>
-          <Image src='/logo.png' />
-          {' '}Admin Page
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
+  <div class="ui left aligned container" style={style.plainContainer}>
+  <div class="ui very relaxed list">
 
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
+    <div class="item">
+      <div class="ui grid">
+        <div class="one wide column">
+          <img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/daniel.jpg" />
+        </div>
+
+        <div class="eight wide column ">
+          <div class="content left aligned">
+            <a class="header">Daniel Lamborghini</a>
+            <div class="description">2 <i class="like outline icon"></i> - today, 11:56am</div>
+          </div>
+        </div>
+
+        <div class="one wide column">
+          <DropdownExampleMenuDirection />
+        </div>
+        <div class="ten wide column">
+          <div class="content">Where I can make a purchase online via cashback ?</div>
+          <Divider inverted />
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <div class="ui grid">
+        <div class="one wide column">
+          <img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/stevie.jpg" />
+        </div>
+
+        <div class="eight wide column ">
+          <div class="content left aligned">
+            <a class="header">Stevie Feliciano</a>
+            <div class="description">0 <i class="like outline icon"></i> - today, 11:56am</div>
+          </div>
+        </div>
+
+        <div class="one wide column">
+          <DropdownExampleMenuDirection />
+        </div>
+        <div class="ten wide column">
+          <div class="content">Are you good ?</div>
+          <Divider inverted />
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <div class="ui grid">
+        <div class="one wide column">
+          <img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/elliot.jpg" />
+        </div>
+
+        <div class="eight wide column ">
+          <div class="content left aligned">
+            <a class="header">Elliot Fu</a>
+            <div class="description">1 <i class="like outline icon"></i> - today, 11:56am</div>
+          </div>
+        </div>
+
+        <div class="one wide column">
+          <DropdownExampleMenuDirection />
+        </div>
+        <div class="ten wide column">
+          <div class="content">How are you doing ?</div>
+          <Divider inverted />
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 );
 
